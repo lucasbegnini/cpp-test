47fcb7e2ea23e472eb95676302c5b41b703cdd14
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-20 13:00:45 -0300

    open_file by argvs

--- challenges/anagram/anagram.cpp
+++ challenges/anagram/anagram.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
-#include <algorithm>
 #include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+using std::string;
+
+void open_file(char* path)
+{
+    ifstream is;
+    string line;
+    ifstream myfile (path);
+    if (myfile.is_open())
+    {
+        while (! myfile.eof())
+        {
+            getline (myfile,line);
+            cout << line << endl;
+        }
+        myfile.close();
+    }
+}
 
 int main(int argc, char** argv)
 {
-	return 0;
+    if(argc == 1) { // Sem parametros
+        cout << "Parametros faltando\n";
+        return 0;
+    }
+
+    open_file(argv[1]);
+    return 0;
 }
