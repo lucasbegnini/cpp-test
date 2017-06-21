b72581d893f3127439a58339c821b70c799a76bf
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-21 11:48:58 -0300

    open file and save numbers in array

--- challenges/functional_anarchy/functional_anarchy.cpp
+++ challenges/functional_anarchy/functional_anarchy.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
 #include <numeric>
 
+using namespace std;
+using std::fstream;
+using std::string;
+
+void open_file(char* path)
+{
+    vector<int> results;
+    string line;
+    int aux;
+    ifstream myfile (path);
+    if (myfile.is_open())
+    {
+
+        while(!myfile.eof())
+        {
+            getline (myfile,line);
+            aux = atoi(line.c_str());
+            results.push_back(aux);
+        }
+    }
+    myfile.close();
+}
+
+
 int main(int argc, char** argv)
 {
-	return 0;
+    if(argc == 1) {
+        cout << "Parameters missing\n";
+        return 0;
+    }
+
+    open_file(argv[1]);
+    return 0;
 }
