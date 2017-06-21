4b04ab5e55e0addcd29ddcae30c91a324273dc3e
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-21 20:58:58 -0300

    transform functions in class

--- challenges/anagram/anagram.cpp
+++ challenges/anagram/anagram.cpp
@@ -13,55 +13,64 @@ using std::ifstream;
 using std::ios;
 using std::string;
 
-int numberOfLines;
-
-void check_anagram(string _word1, string _word2)
+class Anagram
 {
-    string word1 = _word1;
-    string word2 = _word2;
-    if(word1.size() == word2.size())
-    {
-        sort(word1.begin(),word1.end());
-        sort(word2.begin(),word2.end());
-        if(word1 == word2 )
-            cout << _word1 << " is an anagram of " << _word2  << endl;
-        else
-            cout << _word1 << " is not an anagram of " << _word2  << endl;
-    }else{
-        cout << _word1 << " is not an anagram of " << _word2  << endl;
-    }
-}
+public:
+    Anagram()
+    {}
+    int numberOfLines;
 
-void split_variable(const char* line)
-{
-    //Split line
-    std::vector<std::string> result;
-    std::istringstream iss(line);    
-    for(std::string s; iss >> s;)
+    void check_anagram(string _word1, string _word2)
     {
-        result.push_back(s);
-    }
-    check_anagram(result[0],result[1]);
-}
-
-void open_file(char* path)
-{
-    ifstream is;
-    string line;
-    ifstream myfile (path);
-    if (myfile.is_open())
-    {
-        if(getline(myfile, line))
-            numberOfLines = atoi(line.c_str());
-
-        for(int i = 0; i < numberOfLines; i++)
+        string word1 = _word1;
+        string word2 = _word2;
+        if(word1.size() == word2.size())
         {
-            getline (myfile,line);
-            split_variable(line.c_str());          
+            sort(word1.begin(),word1.end());
+            sort(word2.begin(),word2.end());
+            if(word1 == word2 )
+                cout << _word1 << " is an anagram of " << _word2  << endl;
+            else
+                cout << _word1 << " is not an anagram of " << _word2  << endl;
+        }else{
+            cout << _word1 << " is not an anagram of " << _word2  << endl;
         }
-        myfile.close();
     }
-}
+
+
+    int split_variable(const char* line)
+    {
+        //Split line
+        std::vector<std::string> result;
+        std::istringstream iss(line);
+        for(std::string s; iss >> s;)
+        {
+            result.push_back(s);
+        }
+        check_anagram(result[0],result[1]);
+    }
+
+    int open_file(char* path)
+    {
+        string line;
+        ifstream myfile (path);
+        if (myfile.is_open())
+        {
+            if(getline(myfile, line))
+                numberOfLines = atoi(line.c_str());
+
+            for(int i = 0; i < numberOfLines; i++)
+            {
+                getline (myfile,line);
+                split_variable(line.c_str());
+            }
+            myfile.close();
+        }else{
+            return -1;
+        }
+    }
+};
+
 
 int main(int argc, char** argv)
 {
@@ -70,6 +79,8 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    open_file(argv[1]);
+    Anagram _anagram;
+    _anagram.open_file(argv[1]);
+
     return 0;
 }
