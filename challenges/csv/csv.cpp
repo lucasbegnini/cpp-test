9095bddff76c1ebe8817d7976029a99c0943ba01
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-21 09:00:54 -0300

    open file and read number of lines to csv project

--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -3,8 +3,33 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <fstream>
+
+int numberOfLines;
+using namespace std;
+using std::ifstream;
+
+void open_file(char* path)
+{
+    string line;
+    ifstream myfile (path);
+    if (myfile.is_open())
+    {
+        if(getline(myfile, line))
+            numberOfLines = atoi(line.c_str());
+        myfile.close();
+    }
+}
 
 int main(int argc, char** argv)
 {
+    if(argc == 1) {
+        cout << "Parameters missing\n";
+        return 0;
+    }
+
+    open_file(argv[1]);
+    return 0;
+
 	return 0;
 }
