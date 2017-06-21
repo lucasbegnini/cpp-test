a4806f7c3027d80e8b08435b04e15bd9188481f1
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-21 13:31:45 -0300

    read file and get numbers of degrees

--- challenges/stairway_to_heaven/stairway_to_heaven.cpp
+++ challenges/stairway_to_heaven/stairway_to_heaven.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
+#include <stdlib.h>
+
+
+using namespace std;
+using std::ifstream;
+using std::string;
+
+int heavenDegrees;
+
+void read_file(char* path)
+{
+    string line;
+    ifstream myfile (path);
+    if (myfile.is_open())
+    {
+        if(getline(myfile, line))
+            heavenDegrees = atoi(line.c_str());
+
+        myfile.close();
+    }
+}
 
 int main(int argc, char** argv)
 {
+    if(argc == 1) {
+        cout << "Parameters missing\n";
+        return 0;
+    }
+
+    read_file(argv[1]);
 	return 0;
 }
