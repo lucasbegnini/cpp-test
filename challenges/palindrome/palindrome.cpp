46821053123f3ebd5b4dc115f01af01138213621
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-22 14:34:25 -0300

    palindrome function

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+class Palindrome
+{
+public:
+    int check_palindrome(string in)
+    {
+        int reverse = 0;
+        for(int i =0; i < in.size()/2; i++)
+        {
+            reverse = in.length() - i -1;
+            if(in[i] != in[reverse])
+                return 0;
+        }
+        return 1;
+    }
+
+private:
+
+};
 int main(int argc, char** argv)
 {
 	return 0;
