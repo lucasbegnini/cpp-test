343ad90ac97f6f3c341e4f1ed88e13661b340b2f
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-23 09:05:39 -0300

    reading numberOfQueries

--- challenges/classroom/classroom.cpp
+++ challenges/classroom/classroom.cpp
@@ -4,8 +4,21 @@
 #include <map>
 #include <list>
 #include <vector>
+class Classroom
+{
+    int numberOfQueries;
 
+};
 int main(int argc, char** argv)
 {
+    if(argc == 1)
+    {
+        Classroom classroom;
+        cin >> classroom.numberOfQueries;
+        while(classroom.numberOfQueries--)
+        {
+
+        }
+
 	return 0;
 }
