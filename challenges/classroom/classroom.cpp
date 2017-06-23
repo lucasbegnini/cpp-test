ddb641fccbf208fa5d589a56c6fbd81eb7e946fa
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-23 09:48:25 -0300

    adding students

--- challenges/classroom/classroom.cpp
+++ challenges/classroom/classroom.cpp
@@ -20,16 +20,41 @@ public:
     }
     void splitQueries()
     {
-        std::vector<std::string> result;
         std::istringstream iss(query);
         for(std::string s; iss >> s;)
         {
-            result.push_back(s);
+            result.push_back(atoi(s.c_str()));
         }
+    }
+
+    void executeQueries()
+    {
+        if(result[0] == 1)
+        {
+           addStudents(result[1]);
+
+        }
+
+        if(result[0] == 2)
+        {
+            //consult student in present list in the given position
+        }
+    }
+
+    void addStudents(int numberOfStudents)
+    {
+        string auxStudent;
+        for(int i = 0; i< numberOfStudents;i++)
+        {
+            getline(cin, auxStudent, '\n');
+            students.push_back(auxStudent);
+        }
+        sort(students.begin(),students.end());
     }
 private:
     string query;
     vector<int> result;
+    vector<string> students;
 };
 int main(int argc, char** argv)
 {
@@ -41,6 +66,7 @@ int main(int argc, char** argv)
         {
             classroom.getQueries();
             classroom.splitQueries();
+            classroom.executeQueries();
 
         }
 
