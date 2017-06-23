5b8925ce21782f99660cd89b0fe2dccf7195672e
Lucas Begnini <lucasbegnini@gmail.com>
2017-06-23 09:15:09 -0300

    split queries

--- challenges/classroom/classroom.cpp
+++ challenges/classroom/classroom.cpp
@@ -4,10 +4,32 @@
 #include <map>
 #include <list>
 #include <vector>
+#include <sstream>
+
+using namespace std;
+using std::string;
 class Classroom
 {
+public:
     int numberOfQueries;
 
+    void getQueries()
+    {
+        cin.ignore();
+        getline(cin, query, '\n');
+    }
+    void splitQueries()
+    {
+        std::vector<std::string> result;
+        std::istringstream iss(query);
+        for(std::string s; iss >> s;)
+        {
+            result.push_back(s);
+        }
+    }
+private:
+    string query;
+    vector<int> result;
 };
 int main(int argc, char** argv)
 {
@@ -17,8 +39,11 @@ int main(int argc, char** argv)
         cin >> classroom.numberOfQueries;
         while(classroom.numberOfQueries--)
         {
+            classroom.getQueries();
+            classroom.splitQueries();
 
         }
 
-	return 0;
+        return 0;
+    }
 }
